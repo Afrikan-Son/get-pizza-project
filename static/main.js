bca2030c41b15c9f71c4da0476e0209d843d3954
MarkWanjauMoringa <mark.wanjau@student.moringaschool.com>
2022-01-16 23:18:22 +0300

    toppings js

--- static/main.js
+++ static/main.js
@@ -5,3 +5,31 @@ function Order (size, crust, toppings, quantity, deliveryCost) {
     this.quantity = quantity;
     this.deliveryCost = deliveryCost;
   };
+
+  const toppingsSmall = {
+    pepperoni: 90,
+    bacon: 60,
+    pineapple: 50,
+    pepper: 70
+  };
+  
+  const toppingsMedium = {
+    pepperoni: 130,
+    bacon: 100,
+    pineapple: 90,
+    pepper: 110
+  };
+  
+  const toppingsLarge = {
+    pepperoni: 190,
+    bacon: 160,
+    pineapple: 100,
+    pepper: 170
+  };
+  
+  const pizzaPrices = {
+    small: 500,
+    medium: 700,
+    large: 1000
+  };
+  
